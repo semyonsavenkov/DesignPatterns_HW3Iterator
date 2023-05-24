240d22dae652e00b87a13bb52b9ede62907bbd3a
semyonsavenkov <semyonsavenkov@gmail.com>
2023-05-24 23:15:08 +0300

    method next() fixed

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,51 +1,30 @@
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
-    private List<Integer> numberList;
+    private int min;
+    private int max;
 
-    public Randoms (Integer min, Integer max) {
+    public Randoms(Integer min, Integer max) {
         random = new Random();
-        numberList = new ArrayList<>();
-        for (int i = 1; i<=30; i++) {
-            this.addRandomNumber(min, max);
-        }
-    }
-
-    public Randoms addRandomNumber (int min, int max) {
-        numberList.add(random.nextInt(min, max));
-        return this;
+        this.min = min;
+        this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
 
-            int next = 0;
-
             @Override
             public boolean hasNext() {
-                if (next < numberList.size())
-                {
-                    return true;
-                }
-                else {
-                    return false;
-                }
+                return true;
             }
 
             @Override
             public Integer next() {
-                if (next < numberList.size())
-                {
-                    Integer result = numberList.get(next);
-                    next++;
-                    return result;
-                }
-                return null;
+                Integer result = random.nextInt(min, max);
+                return result;
             }
         };
     }
